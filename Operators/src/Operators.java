2d28cb4740507390964f924772ec553a22308d6d
unaisvatts <unaisvatts@gmail.com>
2016-12-09 06:49:01 +0530

    Add files via upload

---
+++ Operators/src/Operators.java
@@ -0,0 +1,18 @@
+
+public class Operators {
+
+	public static void main(String[] args) {
+		int a=20,b=10,c,d,e;
+		b=a-- - --a;
+		System.out.println(b);
+		c=a--;
+		System.out.println(c);
+		d=a>>2;
+		System.out.println(d);
+		e=a&b;
+		System.out.println(e);
+		
+
+	}
+
+}
